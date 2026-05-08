7e2170ee25ddcfe1b6adb5b17155bd335d774b00
ahmed-elarosi <cabry95@protonmail.com>
2026-05-08 15:41:01 +0200

    add solution for isAngram

--- 01-arrays-and-hashing/valid_anagram.py
+++ 01-arrays-and-hashing/valid_anagram.py
@@ -1,2 +1,12 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool: ...
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) == len(t):
+            if sorted(s) == sorted(t):
+                return True
+        return False
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.isAnagram("racecar", "carrace"))
+    print(solution.isAnagram("jar", "jam"))
