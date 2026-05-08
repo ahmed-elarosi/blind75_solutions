ac27a023ffef9b1df20f505fb8dc85b05b64c871
ahmed-elarosi <cabry95@protonmail.com>
2026-05-08 15:42:29 +0200

    add new solution for isAngram

--- 01-arrays-and-hashing/valid_anagram.py
+++ 01-arrays-and-hashing/valid_anagram.py
@@ -1,9 +1,19 @@
+from typing import Counter
+
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) == len(t):
+#             if sorted(s) == sorted(t):
+#                 return True
+#         return False
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) == len(t):
-            if sorted(s) == sorted(t):
-                return True
-        return False
+        if len(s) != len(t):
+            return False
+
+        return Counter(s) == Counter(t)
 
 
 if __name__ == "__main__":
